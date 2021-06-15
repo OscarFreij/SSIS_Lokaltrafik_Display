59a91fa32101a4a13732c04065513ab1e61128f9
oscar <oscar@OTGCG-Server-1.OTGCG-Server>
2021-06-15 04:35:06 +0200

    Suppressed warning message from simplexml_load_string in Index

--- public_html/index.php
+++ public_html/index.php
@@ -4,5 +4,5 @@
 include "../private_html/Container.php";
 $container = new Container();
 
-$data = simplexml_load_string($container->DB()->GetTimeTableData()[0]['xmlData']);
+$data = @simplexml_load_string($container->DB()->GetTimeTableData()[0]['xmlData']);
 echo "<pre>"; print_r($data); exit;    
