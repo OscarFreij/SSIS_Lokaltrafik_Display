0dcd8f5d6cbf2ea390f0fdaa4a204026c6cedc26
oscar <oscar@OTGCG-Server-1.OTGCG-Server>
2021-06-16 05:06:06 +0200

    Starting to add XPath querries and debug display of data.

--- public_html/index.php
+++ public_html/index.php
@@ -1,10 +1,37 @@
 <?php
 
+use function PHPSTORM_META\type;
 
 include "../private_html/Container.php";
 $container = new Container();
 
-foreach ($container->DB()->GetTimeTableData() as $key => $value) {
-    $data = @simplexml_load_string($value['xmlData']);
-    echo "<pre style='border-bottom: dotted black 8px; padding-bottom: 8px;'>"; print_r($data); exit;        
+$dbReturn = $container->DB()->GetTimeTableData();
+
+foreach ($dbReturn as $key => $value) {
+    echo($value['callId']);
+    $data = new DOMDocument();
+    $data->createElementNS("https://api.resrobot.se/xsd?hafasRestDepartureBoard.xsd", "hafas_rest_v1");
+    $XMLToLoad = str_replace("hafas_rest_v1","https://api.resrobot.se/xsd?hafasRestDepartureBoard.xsd",$value['xmlData']);
+    $data->loadXML($XMLToLoad);
+    //$data->createElementNS("https://api.resrobot.se/xsd?hafasRestDepartureBoard.xsd", "hafas_rest_v1");
+    $xpath = new DOMXPath($data);
+    $xpath->registerNamespace("xmlns", "https://api.resrobot.se/xsd?hafasRestDepartureBoard.xsd");
+    
+
+    $printData = $xpath->query("//xmlns:Departure");
+
+    echo "<pre style='border-bottom: dotted black 8px; padding-bottom: 8px;'>";
+    if ($printData == FALSE)
+    {
+        echo("ERROR: printData equal to FALSE");
+    }
+    else
+    {
+        foreach ($printData as $key => $value) {
+            print_r($value);
+        }
+    }
+    echo "</pre><br>";
 }
+
+exit;
