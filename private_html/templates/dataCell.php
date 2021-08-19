9d7bfc4e5a0dc86890ab1bcc7b8f8bd66e4d50b0
oscar <oscar@OTGCG-Server-1.OTGCG-Server>
2021-08-19 12:37:42 +0200

    added basic CSS

--- private_html/templates/dataCell.php
+++ private_html/templates/dataCell.php
@@ -1,10 +1,85 @@
-<div class="dataCell" data-state="normal" data-late="false">
-    <?php
-    foreach ($element as $key => $value) {
+<?php
+
+$state = "normal";
+$late = false;
+
+$directionName = $element['direction'];
+$lineName = $element['lineName'];
+$timeUntillDeparture = "Ingen Data!";
+$departureTime = "Ingen Data!";
+
+$actualDepartureTime = "";
+
+$a = new DateTime();
+$a->setTimestamp($element['unixTimeStamp']);
+$a = $a->format('H:i');
+if ($element['rtUnixTimeStamp'] != "")
+{
+    
+    $actualDepartureTime = $element['rtUnixTimeStamp'];
+    if ($element['rtUnixTimeStamp'] > $element['unixTimeStamp'])
+    {
+        $late = true;
+        $b = new DateTime();
+        $b->setTimestamp($element['rtUnixTimeStamp']);
+        $b = $b->format('H:i');
+    }
+}
+else
+{
+    $actualDepartureTime = $element['unixTimeStamp'];
+}
+$dateTimeNow = new DateTime();
+$timeDiffMedium = $dateTimeNow->diff(new DateTime("@".strval(strtotime('-15 minutes', $actualDepartureTime)))); // -1 extra due to stupid clock
+$timeDiffShort =  $dateTimeNow->diff(new DateTime("@".strval(strtotime('-5 minutes', $actualDepartureTime)))); // -1 extra due to stupid clock
+$timeDiffDeparture =  $dateTimeNow->diff(new DateTime("@".strval($actualDepartureTime)));
+
+if (!$timeDiffDeparture->h == 0 && !$timeDiffDeparture->invert)
+{
+    $timeUntillDeparture = "60+ min";
+}
+else
+{
+    $timeUntillDeparture = $timeDiffDeparture->i." min";
+}
+
+if ($timeDiffDeparture->i == 0 && !$timeDiffDeparture->invert)
+{
+    $state = "critical";
+    $timeUntillDeparture = "NU";
+}
+else if ($timeDiffDeparture->invert)
+{
+    $state = "passed";
+}
+else if ($timeDiffShort->invert ||$timeDiffDeparture->i == 5)
+{
+    $state = "high";
+}
+else if ($timeDiffMedium->invert || $timeDiffDeparture->i == 15)
+{
+    $state = "low";
+}
+
+
+?>
+
+<div class="dataCell" state="<?=$state?>" late="<?=$late?>">
+    <span><?=$directionName?></span>
+    <span class="time"><?=$timeUntillDeparture?></span>
+    <span><?=$lineName?></span>
+    <?php 
+    if ($late)
+    {
         ?>
-        <span>[<?=$key?>] = <?=$value?></span><br>
+        <span class="time"><span class="time time-old"><?=$a?></span> - <?=$b?></span>
+        <?php
+    }
+    else
+    {
+        ?>
+        <span class="time"><?=$a?></span>
         <?php
     }
     ?>
 </div>
-<br>
--- public_html/index.php
+++ public_html/index.php
@@ -5,8 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Exo:wght@500&family=Press+Start+2P&family=Righteous&family=Roboto&display=swap" rel="stylesheet">
+
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="static/js/main.js"></script>
+    <link rel="stylesheet" href="static/css/master.css">
 </head>
 <body>
     <div id="dataBox">
