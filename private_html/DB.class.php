39dfaa90515e7745996efe5452b57eb3e0eae6fb
OscarFreij <otg020313@gmail.com>
2021-12-18 00:53:57 +0100

    Added travleTime restriction to timeTable gathering and dataOutput

--- private_html/DB.class.php
+++ private_html/DB.class.php
@@ -160,13 +160,32 @@ class DB
 
     public function GetTimeTableData($id, $maxElements = 99)
     {
+        try
+        {
+            $stmt = $this->pdo->prepare("SELECT stops.travleTime FROM stops JOIN callTime ON callTime.stopId = stops.id WHERE callTime.id = $id");
+            $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $stmt->execute();
+            $data = $stmt->fetchAll();
+            $travleTime = "-1";
+            
+            if (strval($data[0]['travleTime']) != "0")
+            {
+                $travleTime = "+".$data[0]['travleTime'];
+            }
+            
+        }
+        catch (PDOException $e1)
+        {
+            throw new Exception("PDO TimeTable TravleTime Gathering Error: ".$e1->getMessage(), 1);
+        }
+
         try 
         {
             $dateTimeNow = new DateTime();
-            $currentTimestamp = strtotime('-1 minutes', $dateTimeNow->getTimestamp());
+            $currentTimestamp = strtotime("$travleTime minutes", $dateTimeNow->getTimestamp());
 
             error_log("Attempting to gather XMLData from DB.", 0);
-            $stmt = $this->pdo->prepare("SELECT timeTable.collectionUnixTimeStamp, timeTable.direction, timeTable.lineName, timeTable.unixTimeStamp, timeTable.rtUnixTimeStamp FROM timeTable JOIN callTime ON callTime.id = timeTable.callId WHERE timeTable.callId = $id && ((`unixTimeStamp` > $currentTimestamp && `rtUnixTimeStamp` IS NULL) || (`rtUnixTimeStamp` > $currentTimestamp)) LIMIT $maxElements;");
+            $stmt = $this->pdo->prepare("SELECT timeTable.collectionUnixTimeStamp, timeTable.direction, timeTable.lineName, timeTable.unixTimeStamp, timeTable.rtUnixTimeStamp, stops.travleTime FROM timeTable JOIN callTime ON callTime.id = timeTable.callId JOIN stops ON callTime.stopId = stops.id WHERE timeTable.callId = $id && ((`unixTimeStamp` > $currentTimestamp && `rtUnixTimeStamp` IS NULL) || (`rtUnixTimeStamp` > $currentTimestamp)) LIMIT $maxElements;");
             $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
             $stmt->execute();
             $data = $stmt->fetchAll();
